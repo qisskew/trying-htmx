f73493d34f3e28def8fa64284b5f39588bd43ee7
qisskew <maytoday1@mail.ru>
2024-01-10 21:57:56 +0300

    add GET request to fetch users

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ app.use(express.static('public')); //static folder
 app.use(express.urlencoded({extended: true})); // parse URL encoded body (HTML)
 app.use(express.json()); // parse JSON body (API)
 
+// handle GET request to fetch 
+app.get('/users', (req, res) => {
+    const users = [
+        {id: 1, name: 'Mikhail Soroka'},
+        {id: 2, name: 'Antony MacDon'},
+        {id: 3, name: 'Jenifer Tower'}
+    ];
+
+    res.send(`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join('')}
+        </ul>
+    `);
+})
+
 app.listen(8000, () => {
     console.log('Server listening on port 8000');
 });
