e47355baf2ca9f351d4ca43beea28ef8141e6148
qisskew <maytoday1@mail.ru>
2024-01-10 22:05:22 +0300

    add async get users

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ app.use(express.urlencoded({extended: true})); // parse URL encoded body (HTML)
 app.use(express.json()); // parse JSON body (API)
 
 // handle GET request to fetch 
-app.get('/users', (req, res) => {
-    const users = [
-        {id: 1, name: 'Mikhail Soroka'},
-        {id: 2, name: 'Antony MacDon'},
-        {id: 3, name: 'Jenifer Tower'}
-    ];
+app.get('/users', async(req, res) => {
+    // const users = [
+    //     {id: 1, name: 'Mikhail Soroka'},
+    //     {id: 2, name: 'Antony MacDon'},
+    //     {id: 3, name: 'Jenifer Tower'}
+    // ];
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
 
     res.send(`
         <h1 class="text-2xl font-bold my-4">Users</h1>
